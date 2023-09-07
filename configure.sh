e0d56c85783259e41b605c62ab137ba6d9106940
Darktron <omyp12@gmail.com>
2023-09-06 21:54:42 -0400

    Added config files for execution.
    added start.sh, config.json, configure.sh

--- configure.sh
+++ configure.sh
@@ -1,6 +1,10 @@
 # To change the cuda arch, edit Makefile.am and run ./build.sh
-
-extracflags="-O3 -ffinite-loops -ffast-math -mfix-cortex-a53-835769 -D_REENTRANT -falign-functions=16 -fomit-frame-pointer -fpic -pthread -flto -fuse-ld=lld -fno-stack-protector"
-
-./configure CXXFLAGS="-Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -Wl,-hugetlbfs-align -funroll-loops -finline-functions $extracflags -march=armv8-a+crypto -mtune=cortex-a53" CFLAGS="-finline-functions -Wl,-hugetlbfs-align -march=armv8-a+crypto -mtune=cortex-a53  -Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize  $extracflags -mllvm -enable-loop-distribute"   CXX=clang++ CC=clang LDFLAGS="-v -flto  -Wl,-hugetlbfs-align" 
-
+./configure
+ARCH="-march=native"
+CORE="-mtune=native"
+OPTI="-Ofast -enable-loop-distribute -ffinite-loops -ffast-math -D_REENTRANT -finline-functions -falign-functions=64 -fomit-frame-pointer -fpic -pthread -flto -fuse-ld=lld -fno-stack-protector"
+CXXFLAGS="-Wl,-hugetlbfs-align -funroll-loops $OPTI $ARCH $CORE"
+CFLAGS="-Wl,-hugetlbfs-align -funroll-loops $OPTI $ARCH $CORE"
+LDFLAGS="-Ofast -v -flto -Wl,-hugetlbfs-align"
+CXX=clang++
+CC=clang
