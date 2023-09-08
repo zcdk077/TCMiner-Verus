f7d640b80a31d5f0dcc79a5b1a75223b1a14ae47
Darktron <omyp12@gmail.com>
2023-09-08 07:43:30 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,10 +1,14 @@
-# To change the cuda arch, edit Makefile.am and run ./build.sh
-./configure
-ARCH="-march=native"
+#!/bin/bash
+
+ARCH="-march=armv8-a+crypto"
 CORE="-mtune=native"
-OPTI="-Ofast -Wl,-hugetlbfs-align -funroll-all-loops -ffinite-loops -ffast-math -D_REENTRANT -finline-functions -falign-functions=64 -fomit-frame-pointer -fpic -pthread -flto -fuse-ld=lld -fno-stack-protector -mllvm -enable-loop-distribute"
-CXXFLAGS="$ARCH $CORE $OPTI"
-CFLAGS="$ARCH $CORE $OPTI"
-LDFLAGS="-v -flto -Wl,-hugetlbfs-align"
-CXX=clang++
-CC=clang
+OPTI="-Ofast -Wl,-hugetlbfs-align -funroll-all-loops -ftree-v
+ectorize -ffinite-loops -ffast-math -D_REENTRANT -finline-fun
+ctions -falign-functions=16 -fomit-frame-pointer -fpic -pthre
+ad -flto -fuse-ld=lld -fno-stack-protector -mllvm -enable-loo
+p-distribute"
+
+./configure CXXFLAGS="$ARCH $CORE $OPTI" CFLAGS="$ARCH $CORE
+$OPTI" \
+CXX=clang++ CC=clang  LDFLAGS="-v -pthread -flto -Wl,-hugetlb
+fs-align -Wl,--no-as-needed"
