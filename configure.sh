f51bbf8fa305189cfacb9427e95d6a1590b87ae2
Darktron <omyp12@gmail.com>
2023-09-08 07:46:42 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-ARCH="-march=armv8-a+crypto"
+ARCH="-march=armv8.7-a+crypto"
 CORE="-mtune=native"
-OPTI="-Ofast -Wl,-hugetlbfs-align -funroll-all-loops -ftree-v
-ectorize -ffinite-loops -ffast-math -D_REENTRANT -finline-fun
-ctions -falign-functions=16 -fomit-frame-pointer -fpic -pthre
-ad -flto -fuse-ld=lld -fno-stack-protector -mllvm -enable-loo
-p-distribute"
+OPTI="-Ofast -Wl,-hugetlbfs-align -funroll-all-loops -ftree-vectorize -ffinite-loops -ffast-math -D_REENTRANT -finline-functions -falign-functions=16 -fomit-frame-pointer -fpic -pthread -flto -fuse-ld=lld -fno-stack-protector -mllvm -enable-loop-distribute"
 
-./configure CXXFLAGS="$ARCH $CORE $OPTI" CFLAGS="$ARCH $CORE
-$OPTI" \
-CXX=clang++ CC=clang  LDFLAGS="-v -pthread -flto -Wl,-hugetlb
-fs-align -Wl,--no-as-needed"
+./configure CXXFLAGS="$ARCH $CORE $OPTI" CFLAGS="$ARCH $CORE $OPTI" \
+CXX=clang++ CC=clang  LDFLAGS="-v -pthread -flto -Wl,-hugetlbfs-align -Wl,--no-as-needed"
