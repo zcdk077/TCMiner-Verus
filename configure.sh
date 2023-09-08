e19985b7ea5b411ea8ddb41a3964036438cd2b0f
Darktron <omyp12@gmail.com>
2023-09-08 07:48:53 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ARCH="-march=armv8.7-a+crypto"
+ARCH="-march=armv8-a+crypto"
 CORE="-mtune=native"
 OPTI="-Ofast -Wl,-hugetlbfs-align -funroll-all-loops -ftree-vectorize -ffinite-loops -ffast-math -D_REENTRANT -finline-functions -falign-functions=16 -fomit-frame-pointer -fpic -pthread -flto -fuse-ld=lld -fno-stack-protector -mllvm -enable-loop-distribute"
 
