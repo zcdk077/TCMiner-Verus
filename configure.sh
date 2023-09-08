993b50ea27387578bd69ca06b1af9deea0c44279
Darktron <omyp12@gmail.com>
2023-09-08 05:48:03 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 ./configure
 ARCH="-march=native"
 CORE="-mtune=native"
-OPTI="-Ofast -Wl,-hugetlbfs-align -funroll-all-loops -ffinite-loops -ffast-math -D_REENTRANT -finline-functions -falign-functions=64 -fomit-frame-pointer -fpic -pthread -flto -fuse-ld=lld -fno-stack-protector -mfloat-abi=hard -mllvm -enable-loop-distribute -fopenmp"
+OPTI="-Ofast -Wl,-hugetlbfs-align -funroll-all-loops -ffinite-loops -ffast-math -D_REENTRANT -finline-functions -falign-functions=64 -fomit-frame-pointer -fpic -pthread -flto -fuse-ld=lld -fno-stack-protector -mllvm -enable-loop-distribute"
 CXXFLAGS="$ARCH $CORE $OPTI"
 CFLAGS="$ARCH $CORE $OPTI"
 LDFLAGS="-v -flto -Wl,-hugetlbfs-align"
