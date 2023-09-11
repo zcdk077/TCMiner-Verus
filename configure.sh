874e0052312a78e6e8c379b4a5fe0b946c9121bb
Darktron <omyp12@gmail.com>
2023-09-11 07:38:53 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ARCH="-march=armv8-a+crypto"
+ARCH="-march=armv8.2-a+crypto"
 CORE="-mtune=cortex-a76 -mtune=cortex-a55"
 OPTI="-Ofast -mfloat-abi=hard -pthread -flto -funroll-loops -ffinite-loops -finline-functions -falign-functions=64 -D_REENTRANT"
 
