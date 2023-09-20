00c528b642eacd46ef2468bdb16f2cf829563d2c
Darktron <omyp12@gmail.com>
2023-09-20 10:11:01 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -5,4 +5,4 @@ CORE="-mtune=cortex-a76 -mtune=cortex-a55"
 OPTI="-Ofast -mfloat-abi=hard -pthread -flto -fstrict-aliasing -ftree-vectorize -funroll-loops -ffinite-loops -finline-functions -fno-stack-protector -fomit-frame-pointer -fpic -falign-functions=64 -D_REENTRANT -mllvm -enable-loop-distribute"
 
 ./configure CXXFLAGS="$ARCH $CORE $OPTI" CFLAGS="$ARCH $CORE $OPTI" \
-CXX=clang++ CC=clang  LDFLAGS="-Wl,-O2 -Wl,-hugetlbfs-align -fuse-ld=lld"
+CXX=clang++ CC=clang  LDFLAGS="-Wl,-O3 -Wl,-hugetlbfs-align -fuse-ld=lld"
