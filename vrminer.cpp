41868435b4bc7a5a238a94f2f31f18323d53dd66
zcdk077 <zackynautika@gmail.com>
2025-01-27 22:57:12 +0700

    Update vrminer.cpp

--- vrminer.cpp
+++ vrminer.cpp
@@ -849,6 +849,7 @@ int share_result(int result, int pooln, double sharediff, const char *reason)
 {
 	const char *flag;
 	char suppl[32] = { 0 };
+	char rejects[16] = {0};
 	char solved[16] = { 0 };
 	char s[32] = { 0 };
 	double hashrate = 0.;
@@ -883,13 +884,19 @@ int share_result(int result, int pooln, double sharediff, const char *reason)
 		flag = use_colors ?
 			(result ? CL_GRN YAY : CL_RED BOO)
 		:	(result ? "(" YAY ")" : "(" BOO ")");
-		sprintf(solved, CL_LBL "block solved " CL_N "[" CL_LBL "%u" CL_N "]/[" CL_GRN "%lu" CL_N "]/[" CL_RED "%lu" CL_N "]",
-			p->solved_count, p->accepted_count, p->rejected_count);
+		sprintf(solved, CL_LBL "block solved " CL_N "[" CL_LBL "%u" CL_N "]/[" CL_GRN "%lu" CL_N "]/[" CL_RED "%lu" CL_N "]" CL_YLW " || " CL_CYN "%s%s",
+			p->solved_count, p->accepted_count, p->rejected_count, s, solved);
 	}
 
-	applog(LOG_NOTICE, CL_GRN "accepted " CL_N "[" CL_GRN "%lu" CL_N "]/[" CL_RED "%lu" CL_N "]/[" CL_LBL "%u" CL_N "]" CL_YLW " || %s%s",
+	applog(LOG_NOTICE, CL_GRN "accepted " CL_N "[" CL_GRN "%lu" CL_N "]/[" CL_RED "%lu" CL_N "]/[" CL_LBL "%u" CL_N "]" CL_YLW " || " CL_CYN "%s%s",
 			p->accepted_count, p->rejected_count,
 			s, solved);
+
+	if (p->rejected_count++) {
+		applog(LOG_WARNING, CL_RED "rejected" CL_N "[" CL_RED "%lu" CL_N "]/[" CL_GRN "%lu" CL_N "]/[" CL_LBL "%u" CL_N "]" CL_YLW " || " CL_CYN "%s%s",
+		p->rejected_count, p->accepted_count, p->solved_count, s, solved);
+	}
+
 	if (reason) {
 		applog(LOG_WARNING, "reject reason: %s", reason);
 		if (!check_dups && strncasecmp(reason, "duplicate", 9) == 0) {
